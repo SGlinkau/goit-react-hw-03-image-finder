5502f15da6ad56cbd677b523915f3b59c0a6736f
SGlinkau <97282786+SGlinkau@users.noreply.github.com>
2023-10-24 20:09:18 +0200

    PixaBay Api implementation
    Updated NPM and first test of PixaBay API in APP

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,76 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
-// https://pixabay.com/api/?q=cat&page=1&key=40228040-e1deee2d1dbd5acbce038e379&image_type=photo&orientation=horizontal&per_page=12
+export class App extends Component {
+  state = {
+    images: [],
+    searchQuery: '',
+  };
 
-export const App = () => {
-  return (
-    <>
-      <header>
-        <form>
-          <button type="submit">
-            <span>Search</span>
-          </button>
-          <input
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-          />
-        </form>
-      </header>
-      <section className="gallery">
-        <ul>
-          <li class="gallery-item">
-            <img src="" alt="" />
-          </li>
-        </ul>
-      </section>
-    </>
-  );
-};
+  // SPRAWDZENIE CZY KOMPONENT PIXABAY SIĘ ZAMONTOWAŁ
+  componentDidMount() {
+    this.getImagesFromPixabay();
+  }
+
+  // POBRANIE OBRAZÓW Z API PIXABAY
+  getImagesFromPixabay = async () => {
+    try {
+      const apiKey = '40228040-e1deee2d1dbd5acbce038e379';
+      const searchQuery = this.state.searchQuery;
+      const apiUrl = `https://pixabay.com/api/?key=${apiKey}&q=${searchQuery}&image_type=photo`;
+
+      const response = await axios.get(apiUrl);
+
+      if (response.data.hits) {
+        const images = response.data.hits;
+        this.setState({ images });
+      }
+    } catch (error) {
+      console.error('Błąd podczas pobierania obrazów z Pixabay', error);
+    }
+  };
+
+  // WARTOŚĆ JAKICH OBRAZÓW SZUKAMY W INPUT
+  handleInputChange = event => {
+    this.setState({ searchQuery: event.target.value });
+  };
+
+  // WYSZUKIWANIE OBRAZÓW NA BAZIE HASŁA W INPUT
+  handleSearch = event => {
+    event.preventDefault();
+    this.getImagesFromPixabay();
+  };
+
+  render() {
+    return (
+      <>
+        <header>
+          <form onSubmit={this.handleSearch}>
+            <button type="submit">
+              <span>Search</span>
+            </button>
+            <input
+              type="text"
+              autoComplete="off"
+              autoFocus
+              placeholder="Search images and photos"
+              value={this.state.searchQuery}
+              onChange={this.handleInputChange}
+            />
+          </form>
+        </header>
+        <section className="gallery">
+          <ul>
+            {this.state.images.map((image, index) => (
+              <li className="gallery-item" key={index}>
+                <img src={image.previewURL} alt={image.tags} />
+              </li>
+            ))}
+          </ul>
+        </section>
+
+        <section></section>
+      </>
+    );
+  }
+}
