d288f4a84fab70cc7972011e1c5583264a6dd7b7
SGlinkau <97282786+SGlinkau@users.noreply.github.com>
2023-10-24 21:01:29 +0200

    Added pagination
    Added pagination and load more function. Started working on clear button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,45 +5,65 @@ export class App extends Component {
   state = {
     images: [],
     searchQuery: '',
+    isLoading: false,
+    isSearched: false,
+    currentPage: 1,
   };
 
   // SPRAWDZENIE CZY KOMPONENT PIXABAY SIĘ ZAMONTOWAŁ
   componentDidMount() {
-    this.getImagesFromPixabay();
+    if (this.state.isSearched) {
+      this.getImagesFromPixabay();
+    }
   }
 
   // POBRANIE OBRAZÓW Z API PIXABAY
   getImagesFromPixabay = async () => {
+    this.setState({ isLoading: true });
     try {
       const apiKey = '40228040-e1deee2d1dbd5acbce038e379';
       const searchQuery = this.state.searchQuery;
-      const apiUrl = `https://pixabay.com/api/?key=${apiKey}&q=${searchQuery}&image_type=photo`;
+      const page = this.state.currentPage;
+      const perPage = 12;
+      const apiUrl = `https://pixabay.com/api/?key=${apiKey}&q=${searchQuery}&image_type=photo&orientation=horizontal&page=${page}&per_page=${perPage}`;
 
       const response = await axios.get(apiUrl);
 
       if (response.data.hits) {
-        const images = response.data.hits;
-        this.setState({ images });
+        const newImages = response.data.hits;
+        const images = [...this.state.images, ...newImages];
+        this.setState({ images, isLoading: false, isSearched: true });
       }
     } catch (error) {
       console.error('Błąd podczas pobierania obrazów z Pixabay', error);
+      this.setState({ isLoading: false, isSearched: true });
     }
   };
 
   // WARTOŚĆ JAKICH OBRAZÓW SZUKAMY W INPUT
   handleInputChange = event => {
-    this.setState({ searchQuery: event.target.value });
+    this.setState({ searchQuery: event.target.value, isSearched: false });
   };
 
   // WYSZUKIWANIE OBRAZÓW NA BAZIE HASŁA W INPUT
   handleSearch = event => {
     event.preventDefault();
+    this.setState({ images: [], isSearched: true, currentPage: 1 });
+    this.getImagesFromPixabay();
+  };
+
+  // SZUKANIE WIĘKSZEJ ILOŚC OBRAZÓW PO WCIŚNIĘCIU PRZYCISKU
+  handleLoadMore = () => {
+    this.setState(prevState => ({
+      currentPage: prevState.currentPage + 1,
+    }));
     this.getImagesFromPixabay();
   };
 
   render() {
     return (
       <>
+        {/* SEARCH BAR */}
         <header>
           <form onSubmit={this.handleSearch}>
             <button type="submit">
@@ -57,19 +77,34 @@ export class App extends Component {
               value={this.state.searchQuery}
               onChange={this.handleInputChange}
             />
+            <button type="button">clear</button>
           </form>
         </header>
+        {/* IMAGES GALLERY */}
         <section className="gallery">
-          <ul>
-            {this.state.images.map((image, index) => (
-              <li className="gallery-item" key={index}>
-                <img src={image.previewURL} alt={image.tags} />
-              </li>
-            ))}
-          </ul>
+          {this.state.isLoading ? (
+            <p>Proszę czekać, trwa ładowanie obrazów...</p>
+          ) : (
+            <ul>
+              {this.state.images.length > 0 ? (
+                this.state.images.map((image, index) => (
+                  <li className="gallery-item" key={index}>
+                    <img src={image.previewURL} alt={image.tags} />
+                  </li>
+                ))
+              ) : (
+                <p>Proszę podać wartość szukanej frazy</p>
+              )}
+            </ul>
+          )}
         </section>
 
-        <section></section>
+        {/* BUTTON LOAD MORE */}
+        <button type="button" onClick={this.handleLoadMore}>
+          Load more!
+        </button>
+
+        {/* TUTAJ BĘDZIE MODAL */}
       </>
     );
   }
